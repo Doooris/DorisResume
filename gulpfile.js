b7709313005105f5fccf0d10b4efc56c77805e68
Doooris <hxpt2031@gmail.com>
2017-02-21 21:54:59 +0800

    resume generator

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
+var jade = require('gulp-jade')
+var copy = require('gulp-copy')
+var rimrafPromise = require('rimraf-promise')
+var ghPages = require('gulp-gh-pages')
+var fs = require('fs')
+var connect = require('gulp-connect')
+
+gulp.task('resume-sass', function () {
+  gulp.src('src/css/resume.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(connect.reload())
+})
+
+gulp.task('icon-sass', function () {
+  gulp.src('src/css/iconfont.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist/iconfont/'))
+})
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/css/resume.scss', ['resume-sass'])
+  gulp.watch('./src/css/iconfont.scss', ['icon-sass'])
+  gulp.watch('./src/css/components/*.scss', ['resume-sass'])
+})
+
+gulp.task('json2jade', function () {
+  var info = JSON.parse(fs.readFileSync('./info.json', 'utf-8'))
+  var locals = highlight(info)
+  gulp.src('./src/jade/index.jade')
+    .pipe(jade({
+      locals: locals
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
+})
+
+gulp.task('json2jade:watch', function () {
+  gulp.watch('./info.json', ['json2jade'])
+})
+
+function src2dist(dir) {
+  return gulp.src('./src/${dir}/*.*').pipe(gulp.dest('./dist/${dir}/'))
+}
+
+function highlight(locals) {
+  var locals = JSON.stringify(locals)
+  var re = /`(.+?)`/g
+  locals = locals.replace(re, '<strong>$1</strong>')
+  return JSON.parse(locals)
+}
+
+gulp.task('copy', function() {
+  src2dist('iconfont'),
+  src2dist('img'),
+  src2dist('pdf')
+})
+
+gulp.task('clean', function(){
+  rimrafPromise('./dist/')
+})
+
+//gulp.task('deploy', function () {
+//  gulp.src('./dist/**/*')
+//  .pipe(ghPages({
+//    remoteUrl: 'git@github.com:Lxxyx/lxxyx.github.io.git',
+//    branch: 'master',
+//    force: true
+//  }))
+//})
+
+gulp.task('webserver', function () {
+    connect.server({
+        root: './dist',
+        livereload: true,
+        port:9000
+    })
+})
+
+gulp.task('dev', ['icon-sass', 'resume-sass', 'json2jade', 'copy', 'json2jade:watch', 'sass:watch', 'webserver'])
+
+gulp.task('default', ['icon-sass', 'resume-sass', 'json2jade', 'copy'])
---
+++ readme.md
@@ -0,0 +1,28 @@
+#介绍    
+this is a resume generator inspired by Joyee Cheung which used gulp,sass and jade       
+简历生成器作者是阿里云的张秋怡女神，但女神的这个项目在github上找不到了。    
+简历基于Gulp+Scss+Jade。
+
+####运行方式：
+替换info.json的内容，运行gulp即可。    
+生成内容可在dist文件夹查看。    
+
+
+
+####下面是我在使用这个工具制作简历过程中遇到问题后解决方法：   
+>如果没有icon，考虑下载iconfont    
+>仔细资源路径是否设对 
+
+需要自动部署的，运行`gulp deploy`即可，需要在gulpfile中更改为你的地址（！会抹去你github pages内容，慎用！）
+### 
+
+# License
+(MIT License)
+
+Copyright (c) 2015 Lxxyx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
